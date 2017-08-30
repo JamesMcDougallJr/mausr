9afec358f02fee35e0851dac836389bb6ba8219e
JamesMcDougallJr <jmcdouga@ucsd.edu>
2017-08-30 06:25:38 +0000

    idk

---
+++ motorTry2.py
@@ -0,0 +1,27 @@
+import RPi.GPIO as GPIO
+import time
+ain1=16 #purple
+ain2=18 #Yellow
+stby=22
+PWA=7
+GPIO.setmode(GPIO.BOARD)
+GPIO.setwarnings(False)
+#setup
+def setupMotor():
+    
+    GPIO.setup(ain1,GPIO.OUT)
+    GPIO.setup(ain2,GPIO.OUT)
+    GPIO.setup(stby,GPIO.OUT)
+    GPIO.setup(PWA,GPIO.OUT)
+#run the motor
+def run():
+    
+    GPIO.output(stby,GPIO.HIGH)
+    GPIO.output(PWA,GPIO.HIGH)
+
+    GPIO.output(ain1,GPIO.HIGH)
+    GPIO.output(ain2,GPIO.LOW)
+def stop():
+    GPIO.output(ain1,GPIO.LOW)
+    
+                                                                                                                                                                                                                                                                     
