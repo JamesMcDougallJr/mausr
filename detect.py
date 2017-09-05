66ddbbb6fbf23a55825a139d065086c6380285a0
JamesMcDougallJr <jmcdouga@ucsd.edu>
2017-09-05 19:18:22 +0000

    the problem is that the us and re are conflicting since the us time has to be so long.

--- detect.py
+++ detect.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env/python
 import RPi.GPIO as GPIO
 import time
+GPIO.setmode(GPIO.BOARD)
 #from motor import stop, allBack, right, left
 #from speedcontrol import forward,reverse,stop
 bin1 = 16 #purple the left motor
@@ -18,28 +19,26 @@ clk = 13
 dt = 19
 def measureRotations(counter,lastState):
     GPIO.setmode(GPIO.BOARD)
-    GPIO.setup(clk, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
-    GPIO.setup(dt, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+
     clkState = GPIO.input(clk)
     dtState = GPIO.input(dt)
     if clkState != lastState:
         if dtState != clkState:
-            lastState = clkState
-        counter+=1
-    print(counter)
-    return (counter,lastState)
-    
-    
+            counter+=1
+            print(counter)
+    lastState = clkState
+    tup=(counter,lastState)
+    return tup    
 def setup():
+    GPIO.setup(clk, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+    GPIO.setup(dt, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
     GPIO.setwarnings(False)
     GPIO.setup(trig,GPIO.OUT)
     GPIO.setup(echo,GPIO.IN)
-    GPIO.setup(clk, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
-    GPIO.setup(dt, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
 def distance():
     GPIO.setmode(GPIO.BOARD)
     GPIO.output(trig,GPIO.LOW)
-    time.sleep(0.4)
+    time.sleep(0.004)
     GPIO.output(trig,GPIO.HIGH)
     time.sleep(0.00001)
     GPIO.output(trig,GPIO.LOW)
@@ -112,35 +111,33 @@ def right():
 GPIO.setmode(GPIO.BOARD)
 setup()
 setupMotor()
-time.sleep(2)
 runall()
 rightCounter=0
+lastState = GPIO.input(clk)
+GPIO.setmode(GPIO.BOARD)
+GPIO.setup(clk, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+GPIO.setup(dt, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
 counter=0
-laststate = GPIO.input(clk)
-rotationState=0
-LastrotationState=GPIO.input(clk)
+clkLastState = GPIO.input(clk)
 while True:
-    GPIO.setmode(GPIO.BOARD)
-    try:
-        [rotationCounter,rotationstate]=measureRotations(counter,laststate)
-        if rotationstate==LastrotationState:
-            lastrotationState=rotationState            
+    clkState = GPIO.input(clk)
+    dtState = GPIO.input(dt)
+    if clkState != clkLastState:
+        if dtState != clkState:
+             counter+= 1
+             print(counter)
+    clkLastState = clkState
+    """
+    if loop()==0:
+        if rightCounter >=5:
+            rightCounter=0
             allBack()
-            time.sleep(1)
-            left()
-            time.sleep(.5)
-        if loop()==0:
-            if rightCounter >=5:
-                rightCounter=0
-                allBack()
-                time.sleep(2)
-            right()
-            rightCounter+=1
-            time.sleep(0.25)
-        runall()
-    except KeyboardInterrupt:
-        stop()
-        GPIO.cleanup()
+            time.sleep(2)
+        right()
+        rightCounter+=1
+        time.sleep(0.25)
+    runall()
+"""
 
 
 
